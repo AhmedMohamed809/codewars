85b3577f0c09b9ae69adefb620d55ac58f7dd56d
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-10-26 04:39:52 +0100

    add false

--- codewars.js
+++ codewars.js
@@ -333,7 +333,7 @@
 // 
 function isPangram(string){
   const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  let H= true;
+  let H= false;
   for(let i = 0 ; i < alphabet.length;i++){
     let arrayStr= string.split("").filter((x)=>{
       
