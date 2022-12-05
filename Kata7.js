0f8f2a7651b0783aecf7effcc7040469ce3ad577
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-12-05 22:12:02 +0000

    complete Kata 7

--- Kata7.js
+++ Kata7.js
@@ -1,10 +1,22 @@
 // Currying functions: multiply all elements in an array
 
-function multiplyAll(array) {
-  return (number) => {
-    return array.map((el) => el * number);
-  };
-}
+// function multiplyAll(array) {
+//   return (number) => {
+//     return array.map((el) => el * number);
+//   };
+// }
 
-console.log(multiplyAll([1, 2, 3])(2));
+// console.log(multiplyAll([1, 2, 3])(2));
 // multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+//############################################
+//Coding Meetup #11 - Higher-Order Functions Series - Find the average age
+function getAverageAge(list) {
+  let ages = list.map((el) => el.age);
+  console.log(ages);
+  let sumAges = ages.reduce((c, item) => {
+    c = c + item;
+    return c;
+  }, 0);
+  console.log(sumAges);
+  return Math.round(sumAges / ages.length);
+}
